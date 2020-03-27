e3748f88f28f3d11cf839085ff43e23e6a7aa80c
Ankitshah89 <shahdankit@gmail.com>
2020-03-27 17:17:44 -0600

    Added rows to database

--- DB/quarantine.sql
+++ DB/quarantine.sql
@@ -287,7 +287,87 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `quarantinedb`;
-INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `user_image_url`, `create_date`, `user_description`, `enabled`, `role`) VALUES (1, 'Chrissy', 'Rowland', 'frog', 'crispy', NULL, NULL, NULL, 1, 'admin');
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `user_image_url`, `create_date`, `user_description`, `enabled`, `role`) VALUES (1, 'Chrissy', 'Rowland', 'frog', 'crispy', 'https://www.cmgww.com/music/tim/wp-content/uploads/2017/05/timt006.jpg', '2020-01-24', 'Looking for frogs..!!!', 1, 'admin');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `location`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `location` (`id`, `country`, `state`, `city`, `address`, `postal_code`) VALUES (1, 'Afghanistan', 'Kandahar', 'Kandahar haha', '123 Route Misty', '00001');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `host`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `host` (`id`, `host_name`, `email`, `location_id`, `phone_number`, `url`, `url_logo`) VALUES (1, 'Captain Planet', 'abc@xyz.com', 1, '123-456-789 ', 'https://www.eventbrite.com/e/chicken-fight-the-denver-fried-chicken-championships-2020-tickets-92812908909?aff=ebdshpfprimarybucket', 'https://www.simplyrecipes.com/wp-content/uploads/2015/09/roasted-chicken-apricot-glaze-horiz-a-1500.jpg');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `event`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `event` (`id`, `description`, `title`, `event_date`, `event_time`, `link_to_event`, `host_id`, `location_id`, `public`, `event_image_url`, `status`, `prereqs`, `create_date`) VALUES (1, 'Looking for my new baby mama !!', 'Hooker Rank', '2020-03-30', '16:00', 'http://cornhub.com/', 1, 1, 1, 'https://domaingang.com/domain-news/is-cornhub-com-really-worth-365000-dollars/', NULL, 'Love for corns is a must !', '2020-03-27');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `category`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `category` (`id`, `name`, `description`, `category_image_url`) VALUES (1, 'Animal Cams', 'Watch your favorite Frogs correct the British spelling', 'https://www.nationalgeographic.com/content/dam/news/photos/000/842/84214.ngsversion.1422286683017.jpg');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `event_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `event_comment` (`id`, `user_id`, `event_id`, `content`, `create_date`) VALUES (1, 1, 1, 'Whats going on with these events dudeee... I am ready for the hookers...', '2020-03-27');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `event_category`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `event_category` (`category_id`, `event_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `user_event`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `user_event` (`user_id`, `event_id`, `attended`, `rating`, `rating_comment`) VALUES (1, 1, 1, 4, 'Loved the hookers and the frog at the animal zoo. The connection was a little sketch');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `tag`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `quarantinedb`;
+INSERT INTO `tag` (`id`, `user_id`, `tag_name`, `create_date`) VALUES (1, 1, 'hookerswithfrogs', '2020-03-27');
 
 COMMIT;
 
