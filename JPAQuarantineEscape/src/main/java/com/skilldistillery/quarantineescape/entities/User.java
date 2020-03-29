2331901e093d567db191626205f89c08b7a92c33
Ankitshah89 <shahdankit@gmail.com>
2020-03-28 18:07:45 -0600

    Deleated unwanted add and removes from entities

--- JPAQuarantineEscape/src/main/java/com/skilldistillery/quarantineescape/entities/User.java
+++ JPAQuarantineEscape/src/main/java/com/skilldistillery/quarantineescape/entities/User.java
@@ -32,7 +32,7 @@ public class User {
 
 	@Column(name = "user_image_url")
 	private String userImageUrl;
-	
+
 	@Column(name = "create_date")
 	private LocalDate createdAt;
 
@@ -45,17 +45,14 @@ public class User {
 
 	@OneToMany(mappedBy = "user")
 	private List<CategoryComment> categoryComments;
-	
-	@OneToMany(mappedBy="user")
+
+	@OneToMany(mappedBy = "user")
 	private List<UserEvent> userEvents;
-	
-	@OneToMany(mappedBy="user")
-	private List<EventComment> eventComments;	
-	
-	
+
+	@OneToMany(mappedBy = "user")
+	private List<EventComment> eventComments;
 
 	////////////////////////////////////////////
-	
 
 	public List<EventComment> getEventComments() {
 		return eventComments;
@@ -67,6 +64,98 @@ public class User {
 
 	public List<UserEvent> getUserEvents() {
 		return userEvents;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public void setUserEvents(List<UserEvent> userEvents) {
+		this.userEvents = userEvents;
+	}
+
+	public List<CategoryComment> getCategoryComments() {
+		return categoryComments;
+	}
+
+	public void setCategoryComments(List<CategoryComment> categoryComments) {
+		this.categoryComments = categoryComments;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserImageUrl() {
+		return userImageUrl;
+	}
+
+	public void setUserImageUrl(String userImageUrl) {
+		this.userImageUrl = userImageUrl;
+	}
+
+	public String getUserDescription() {
+		return userDescription;
+	}
+
+	public void setUserDescription(String userDescription) {
+		this.userDescription = userDescription;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
 	}
 
 	public User() {
@@ -187,110 +276,8 @@ public class User {
 		return true;
 	}
 
-	public LocalDate getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(LocalDate createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public void setUserEvents(List<UserEvent> userEvents) {
-		this.userEvents = userEvents;
-	}
-
-	
-
-	public List<CategoryComment> getCategoryComments() {
-		return categoryComments;
-	}
-
-	public void setCategoryComments(List<CategoryComment> categoryComments) {
-		this.categoryComments = categoryComments;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getUserImageUrl() {
-		return userImageUrl;
-	}
-
-	public void setUserImageUrl(String userImageUrl) {
-		this.userImageUrl = userImageUrl;
-	}
-
-	
-
-	public String getUserDescription() {
-		return userDescription;
-	}
-
-	public void setUserDescription(String userDescription) {
-		this.userDescription = userDescription;
-	}
-
-	public Boolean getEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-
-	
-
-	
-	
-
 /////////////////////////////////////////
 
-///////////////////////////
+
 
 }
