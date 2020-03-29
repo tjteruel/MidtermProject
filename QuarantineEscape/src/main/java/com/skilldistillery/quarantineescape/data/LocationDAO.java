47621784f77800130bb79305f63a599416758279
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-03-29 17:30:04 -0600

    added locationDAO and Impl

---
+++ QuarantineEscape/src/main/java/com/skilldistillery/quarantineescape/data/LocationDAO.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.quarantineescape.data;
+
+import com.skilldistillery.quarantineescape.entities.Location;
+
+public interface LocationDAO {
+	
+	public Location createLocation(Location location);
+	public boolean deleteLocation(int locationId);
+	public Location updateLocation(Location location, int locationId);
+
+}
