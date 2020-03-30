2b8dda7832fa04e26c268b6431e24812ac0be959
dyoachum <dyoachum@gmail.com>
2020-03-30 10:30:23 -0600

    working on controller for event

--- QuarantineEscape/src/main/java/com/skilldistillery/quarantineescape/controllers/EventController.java
+++ QuarantineEscape/src/main/java/com/skilldistillery/quarantineescape/controllers/EventController.java
@@ -2,8 +2,15 @@ package com.skilldistillery.quarantineescape.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.quarantineescape.data.EventDAO;
+import com.skilldistillery.quarantineescape.entities.Event;
+import com.skilldistillery.quarantineescape.entities.User;
 
 @Controller
 public class EventController {
@@ -11,7 +18,20 @@ public class EventController {
 	@Autowired
 	private EventDAO dao;
 	
-
+	@RequestMapping(path = "findEvent.do", method = RequestMethod.GET, params = "id")
+	public String findEventById(@RequestParam int id, Model model) {
+		String view = "Event";
+		Event event = dao.findEventById(id);
+		model.addAttribute("event", event);
+		return view;
+	}
+	@RequestMapping(path = "createEvent.do", method = RequestMethod.GET)
+	public ModelAndView createEvent(Event event) {
+		dao.createEvent(event);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
+	}
 	
 	
 	
